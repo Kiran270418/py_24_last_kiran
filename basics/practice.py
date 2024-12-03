1c720171f8af0c4fa998d5cff4262daecfa1edf6
Kiran270418 <chkiran14159@gmail.com>
2024-12-02 22:09:37 -0500

    practice

--- basics/practice.py
+++ basics/practice.py
@@ -1,7 +1,13 @@
-number = input("enter value: ")
+# number = input("enter value: ")
 table = {}
-for i in range(1,11):
-    # number = input("enter value: ")
-    table[i] = int(number) * i
-for k,v in table.items():
-    print (f"{number}*{k}={v}")
+def tables(number):
+    for i in range(1,11):
+        # number = input("enter value: ")
+        table[i] = int(number) * i
+    for k,v in table.items():
+        # return (f"{number}*{k}={v}")
+        print(f"{number}*{k}={v}")
+
+tables(5)
+# for k,v in table.items():
+#     print (f"{number}*{k}={v}")
